743b4dfa9c1bfd21284ea3e9f608346c577f4bd9
lokeshpandey <87053268+lokesh144@users.noreply.github.com>
2025-01-11 18:40:21 +0545

    Update Port

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use("/",router);
 //      Handle data processing and saving to the database
 //     res.status(200).json({ message: 'Data received', data });
 // });
+const PORT = process.env.PORT || 3000;
 connectDb().then(()=>{
     app.listen(PORT,()=>{
         console.log(`Server is listening on port ${PORT}`);
