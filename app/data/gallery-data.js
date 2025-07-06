efc132e76b5582d251db16094938898db7bdbd30
lokesh144 <lokeshpan31@gmail.com>
2025-07-06 19:44:01 +0545

    Gallery added

--- app/data/gallery-data.js
+++ app/data/gallery-data.js
@@ -1,56 +1,56 @@
 const galleryData=[
     {
         id:1,
-        image:"https://utfs.io/f/30da111b-6d6f-49ea-8bf1-8b95b773bcdb-1d.jpg",
+        image:"https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3gljXBMkQkzAp8roHmD3Yd9XQlbiwqVh6FTKR",
         name:"School Building",
         category:["all","2072"],
     },
 
     {
         id:2,
-        image:"https://utfs.io/f/111d226c-686e-4509-9dd4-774d7363ada7-oail16.JPG",
+        image:"https://utfs.io/f/111d226c-686e-4509-9dd4-774d7363ada7-oail16.jpg",
         name:"Science Lab",
         category:["all","2073"],
     },
 
     {
         id:3,
-        image:"https://utfs.io/f/7f9a610e-0b6a-4bf4-83ca-d6d9ce46fa16-2c2o.jpg",
+        image:"https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3PuBnE3wJ5NaqbY0kD3zew1S7WmdpchOM9Eiy",
         name:"Nepal First 2073 (GPA-4.00)",
         category:["all","2074"],
     },
 
     {
         id:4,
-        image:"https://utfs.io/f/e94f8d79-d7a0-4faf-b0a3-ae92a5a6075f-1x9zto.JPG",
+        image:"https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3iHIJtFO4mdib58lSzfTZa70KkBps2euLAngU",
         name:"Computer Lab",
         category:["all","2075"],
     },
 
     {
         id:5,
-        image:"https://utfs.io/f/91cdfdf9-1c1b-411a-9534-0716b7811844-fpn7x2.jpg",
+        image:"https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3Ctf1Dsp5O7e1ugcEXvaV02lmGFB9xRWNhyj8",
         name:"OPA Attending COFAS",
         category:["all","2076"],
     },
 
     {
         id:6,
-        image:"https://utfs.io/f/2a39ff1d-76fa-4c1e-ad7b-7b5ff9baa0ac-1h.jpg",
+        image:"https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3OccxibPzm6dvHeoQBswAFfrG8E07yUIZhab3",
         name:"Nepal First Rally",
         category:["all","2077"],
     },
 
     {
         id:7,
-        image:"https://utfs.io/f/85a34ff2-0772-4196-81c9-f0071fe30753-z6oyj3.JPG",
+        image:"https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3vp8UPDtldG6svA2RNP1BpTC4ZJDg5UKmoq90",
         name:"Houseboard Decoration",
         category:["all","2077"],
     },
 
     {
         id:8,
-        image:"https://utfs.io/f/66478e0e-fc61-48d1-a3d2-ad1fca867f64-1f8ih.jpg",
+        image:"https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3kv9aCilMIUy6au25qgHiYPt8JhlRrG9FN3VL",
         name:"Library Hall",
         category:["all","2077"],
     },
