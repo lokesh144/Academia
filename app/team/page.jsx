44ac68f4c505f99592bacf45c6e55f3a429c9646
Adarsh Ghimire <adarshghimire052@gmail.com>
2026-03-11 07:46:22 +0545

    added

--- app/team/page.jsx
+++ app/team/page.jsx
@@ -6,8 +6,9 @@ import {
   IconButton,
   Typography,
 } from "@material-tailwind/react";
+import Navbarr from "../../components/Navbarr";
 
-function TeamCard({ img, name, title }) {
+function TeamCard({ img, name, title, facebook }) {
   return (
     <Card className="rounded-lg h-full flex flex-col overflow-hidden border border-gray-200">
       <CardBody className="text-center p-0 flex flex-col flex-grow">
@@ -39,9 +40,11 @@ function TeamCard({ img, name, title }) {
 
           {/* Social */}
           <div className="flex items-center justify-center mt-4">
-            <IconButton variant="text" color="gray" size="sm">
-              <i className="fa-brands fa-facebook text-base" />
-            </IconButton>
+            <a href={facebook} target="_blank" rel="noopener noreferrer">
+              <IconButton variant="text" color="gray" size="sm">
+                <i className="fa-brands fa-facebook text-base" />
+              </IconButton>
+            </a>
           </div>
         </div>
 
@@ -51,56 +54,74 @@ function TeamCard({ img, name, title }) {
 }
 
 const members = [
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3kPRpYrlMIUy6au25qgHiYPt8JhlRrG9FN3VL", name: "Salina Pun", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3s4TcnagSYoVlxbXDuLFUq1BzA487wKMcgvR5", name: "Homkali Pun Shreesh", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3mTxDZGHMOYJBSRjTwp2taUcNkoEiV7fQ0sIb", name: "Kala Adhikari", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3sHndYrgSYoVlxbXDuLFUq1BzA487wKMcgvR5", name: "Ganga Bhandari", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3JMKiIOesdzUDNaqbrMulL8k4e3CVjAG6y5gR", name: "Rita Nepal", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS32K7jDwGRTAh60R5JIlKn8mcbXyiwYatBLOjF", name: "Sabita Poudel", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3XLh8GEm5D4qQdKiIPwVxHlNn7pA3hWvSousz", name: "Deepa Karki", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS39H4Tz8yXsIMNv5UbkC4epGRgHc82nOdjltow", name: "Kamal Pandey", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3F7JSc2er4dECjxf1Qanp6glomUreDwRIVHyM", name: "Romanch Pun", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3XWvs7Gim5D4qQdKiIPwVxHlNn7pA3hWvSous", name: "Tulsa Rana Shreenet", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3JmgD4iesdzUDNaqbrMulL8k4e3CVjAG6y5gR", name: "Rabina Khatri", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3dlQVWkJA0acClsx9hjqMpoOTtiG7EWJeUH6S", name: "Laxmi Bhandari", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS36phNhhwBURl0bm35tYDKsSqQg8oXrW4unEO7", name: "Sanjana Roka", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3dslont9JA0acClsx9hjqMpoOTtiG7EWJeUH6", name: "Sandeep Khatri", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS31jMYhPGWRgiI3a5MrcApjOVfeYZq4N9kx0Gn", name: "Sushmita Kunwar", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3UJ2wNHIWpeYBnwKamGSyufR2bZTq7z4cxj0X", name: "Pushpa Bhusal", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS37PoOwZqveoPZg2BTClA4NuXsqzndD6tSh1mJ", name: "Sunita Bhattarai", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3xqXeI2MN3Kk5mueI6LHnpcwCG7WTE8zi1bFl", name: "Himal Pachhai", title: "Teacher" },
-  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS33KCvUYh8RYw1KpnAzTsjqELWraul6h0O7CQI", name: "Dipesh Charti", title: "Teacher" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3kPRpYrlMIUy6au25qgHiYPt8JhlRrG9FN3VL", name: "Salina Pun", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3s4TcnagSYoVlxbXDuLFUq1BzA487wKMcgvR5", name: "Homkali Pun Shreesh", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3mTxDZGHMOYJBSRjTwp2taUcNkoEiV7fQ0sIb", name: "Kala Adhikari", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3sHndYrgSYoVlxbXDuLFUq1BzA487wKMcgvR5", name: "Ganga Bhandari", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3JMKiIOesdzUDNaqbrMulL8k4e3CVjAG6y5gR", name: "Rita Nepal", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS32K7jDwGRTAh60R5JIlKn8mcbXyiwYatBLOjF", name: "Sabita Poudel", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3XLh8GEm5D4qQdKiIPwVxHlNn7pA3hWvSousz", name: "Deepa Karki", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS39H4Tz8yXsIMNv5UbkC4epGRgHc82nOdjltow", name: "Kamal Pandey", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3F7JSc2er4dECjxf1Qanp6glomUreDwRIVHyM", name: "Romanch Pun", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3XWvs7Gim5D4qQdKiIPwVxHlNn7pA3hWvSous", name: "Tulsa Rana Shreenet", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3JmgD4iesdzUDNaqbrMulL8k4e3CVjAG6y5gR", name: "Rabina Khatri", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3dlQVWkJA0acClsx9hjqMpoOTtiG7EWJeUH6S", name: "Laxmi Bhandari", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS36phNhhwBURl0bm35tYDKsSqQg8oXrW4unEO7", name: "Sanjana Roka", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3dslont9JA0acClsx9hjqMpoOTtiG7EWJeUH6", name: "Sandeep Khatri", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS31jMYhPGWRgiI3a5MrcApjOVfeYZq4N9kx0Gn", name: "Sushmita Kunwar", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3UJ2wNHIWpeYBnwKamGSyufR2bZTq7z4cxj0X", name: "Pushpa Bhusal", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS37PoOwZqveoPZg2BTClA4NuXsqzndD6tSh1mJ", name: "Sunita Bhattarai", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS3xqXeI2MN3Kk5mueI6LHnpcwCG7WTE8zi1bFl", name: "Himal Pachhai", title: "Teacher", facebook: "https://facebook.com/" },
+  { img: "https://dkw5e4ooeb.ufs.sh/f/QoBGn9AkhKS33KCvUYh8RYw1KpnAzTsjqELWraul6h0O7CQI", name: "Dipesh Charti", title: "Teacher", facebook: "https://facebook.com/" },
+
+  /* 13 Dummy Members */
+  { img: "https://i.pravatar.cc/300?img=1", name: "Ram Sharma", title: "Teacher", facebook: "https://facebook.com/dummy1" },
+  { img: "https://i.pravatar.cc/300?img=2", name: "Sita Thapa", title: "Teacher", facebook: "https://facebook.com/dummy2" },
+  { img: "https://i.pravatar.cc/300?img=3", name: "Hari Karki", title: "Teacher", facebook: "https://facebook.com/dummy3" },
+  { img: "https://i.pravatar.cc/300?img=4", name: "Bina Gurung", title: "Teacher", facebook: "https://facebook.com/dummy4" },
+  { img: "https://i.pravatar.cc/300?img=5", name: "Kiran Magar", title: "Teacher", facebook: "https://facebook.com/dummy5" },
+  { img: "https://i.pravatar.cc/300?img=6", name: "Nabin Rai", title: "Teacher", facebook: "https://facebook.com/dummy6" },
+  { img: "https://i.pravatar.cc/300?img=7", name: "Lila BK", title: "Teacher", facebook: "https://facebook.com/dummy7" },
+  { img: "https://i.pravatar.cc/300?img=8", name: "Prakash Adhikari", title: "Teacher", facebook: "https://facebook.com/dummy8" },
+  { img: "https://i.pravatar.cc/300?img=9", name: "Saraswati GC", title: "Teacher", facebook: "https://facebook.com/dummy9" },
+  { img: "https://i.pravatar.cc/300?img=10", name: "Dipak Shrestha", title: "Teacher", facebook: "https://facebook.com/dummy10" },
+  { img: "https://i.pravatar.cc/300?img=11", name: "Anita Lama", title: "Teacher", facebook: "https://facebook.com/dummy11" },
+  { img: "https://i.pravatar.cc/300?img=12", name: "Ramesh Ale", title: "Teacher", facebook: "https://facebook.com/dummy12" },
+  { img: "https://i.pravatar.cc/300?img=13", name: "Pooja Khatri", title: "Teacher", facebook: "https://facebook.com/dummy13" },
 ];
 
 const Teamn = () => {
   return (
-    <section className="bg-white py-16 px-6">
-      <div className="container mx-auto">
-        <div className="mb-14 text-center">
-          <Typography
-            variant="h1"
-            color="blue-gray"
-            className="text-4xl md:text-[2.8rem] font-bold"
-          >
-            OUR TEAM
-          </Typography>
-        </div>
+    <>
+      <Navbarr />
+      <section className="bg-white py-16 px-6">
+        <div className="container mx-auto">
+          <div className="mb-14 text-center">
+            <Typography
+              variant="h1"
+              color="blue-gray"
+              className="text-4xl md:text-[2.8rem] font-bold"
+            >
+              OUR TEAM
+            </Typography>
+          </div>
 
-        <div
-          className="grid gap-x-6 gap-y-12 
-          grid-cols-1 
-          sm:grid-cols-2 
-          md:grid-cols-3 
-          lg:grid-cols-4 
-          xl:grid-cols-5 
-          items-stretch"
-        >
-          {members.map((member, index) => (
-            <TeamCard key={index} {...member} />
-          ))}
+          <div
+            className="grid gap-x-6 gap-y-12 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4 
+            xl:grid-cols-5 
+            items-stretch"
+          >
+            {members.map((member, index) => (
+              <TeamCard key={index} {...member} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
